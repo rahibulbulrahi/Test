b84a315dca9f7494b243655ab3728ac9e5217365
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2021-03-15 20:17:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<<<<<<< Updated upstream
     	<div class="navbar">
             <nav class="navbar navbar-expand-sm navbar-light bg-light">
                 <a class="navbar-brand" href="#">Navbar</a>
@@ -46,5 +47,28 @@
                 </div>
             </nav>
         </div>
+=======
+    	
+        <p1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, nam. Repudiandae mollitia vel iusto corporis ullam velit a at excepturi. Enim corrupti dolores tempore sint culpa magnam error placeat quasi?
+        Pariatur recusandae et animi dolorum nihil itaque tempore atque exercitationem temporibus doloremque? Voluptatibus, minus veniam aspernatur, assumenda earum ipsam maiores, mollitia unde natus totam ea modi reiciendis corrupti laboriosam incidunt.
+        Corrupti voluptate ullam deserunt. Rerum dolorem unde illum tempora suscipit corrupti voluptatum enim quos exercitationem, dicta fuga sunt doloribus vel corporis. Inventore vero maiores totam aliquam consequuntur voluptatum? Tempore, eum!
+        Earum, ut nisi, ipsum consectetur optio nesciunt porro maiores quibusdam voluptates beatae modi ea perferendis itaque consequuntur animi! Totam expedita sed nostrum beatae quas optio maiores aliquid, quam repellendus dignissimos.
+        A obcaecati cum labore officia maxime! Natus at similique consectetur sapiente quidem ullam laborum, vitae doloremque eos, perferendis repudiandae voluptatum illum beatae nihil cupiditate enim nostrum incidunt ea harum voluptates!</p1>
+
+
+
+        <p1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at beatae doloribus quis reiciendis veniam voluptate enim quasi blanditiis iusto recusandae, qui ullam quod dolor architecto magni et quam eos?
+        Eos voluptas incidunt consectetur amet inventore ut tempora eum, ad qui beatae voluptatibus soluta sint magni minus obcaecati! Nobis commodi voluptatem non quasi similique necessitatibus tempora aliquam repudiandae assumenda delectus!
+        Illum debitis voluptatibus minus, vel nihil quasi deserunt omnis adipisci eaque voluptatem ab optio dolorum eos, ratione laborum minima incidunt itaque consequatur voluptatum atque. Consectetur, architecto aspernatur. Sed, qui aspernatur?
+        Inventore minima facilis aliquam exercitationem aperiam, modi quisquam quas, veritatis ad ipsum sit possimus atque ullam, quos pariatur voluptatum quaerat perspiciatis eius. Soluta quibusdam aspernatur, blanditiis rem earum libero cupiditate?
+        Eius, eos commodi libero itaque quae quo nam, id corporis amet dicta sint mollitia quia exercitationem, aperiam iusto molestias in error officiis incidunt labore quaerat. Saepe voluptate magni doloribus nemo.
+        Labore praesentium dolores odio quam tempora id voluptate quaerat necessitatibus placeat. Facere consectetur nulla temporibus, suscipit consequuntur beatae quae aliquam iure a nobis eos unde ipsam saepe, quas vel incidunt!
+        Laboriosam, dolor odit? Labore molestiae architecto quae facere aperiam animi blanditiis nemo eum, exercitationem perspiciatis? Sunt temporibus esse, eaque iusto dicta qui placeat vitae harum, aut amet quasi eos nostrum!
+        Placeat voluptatem quo voluptatum, delectus ad quia provident libero ipsa! Maiores commodi necessitatibus dolore aut repudiandae qui accusantium, maxime sapiente asperiores rem, velit, repellendus mollitia reiciendis voluptas sed cupiditate nobis!
+        At tempore ea quidem placeat, expedita mollitia delectus earum assumenda sequi ut iste iure asperiores. Architecto, dolore, inventore sapiente asperiores, debitis est vero beatae molestias laboriosam accusantium nemo veniam natus.
+        Tenetur ratione itaque amet. Dolore dicta, quod impedit doloribus porro sequi sapiente nostrum laudantium at adipisci esse perferendis minima pariatur ratione delectus, ad fugit corrupti sed tempora? Labore, nesciunt nobis?</p1>
+
+        
+>>>>>>> Stashed changes
   </body>
 </html>
